2dd6d34bb1314c3041d805e673c7b279c35a4f1e
Garrett Stallsmith <gwstallsmith@gmail.com>
2023-10-03 19:42:09 -0400

    Added timer for go/stop button.

--- game.js
+++ game.js
@@ -13,29 +13,35 @@ class Timer {
     constructor(seconds) {
         this.seconds_ = seconds;
         this.playingRound_ = false;
+        this.intervalId_ = null
     }
 
-    startTimer() {
+    updateCountdown() {
         if(this.seconds_ > 0) {
             this.seconds_--;
-            console.log("timer" + this.seconds_);
+
+            console.log(this.seconds_);
+
         } else {
-            clearInterval(interval);
+            clearInterval(this.intervalId_);
         }
     }
 
-    stopTimer() {
-        if(this.seconds_ > 0) {
-            this.seconds_++
-            console.log("timer" + this.seconds_);
-        } else {
-            clearInterval(interval);
+    start() {
+        if(this.intervalId_ === null) {
+            this.intervalId_ = setInterval(() => this.updateCountdown(), 1000);
+        }
+    }
+
+    stop() {
+        if(this.intervalId_ !== null) {
+            clearInterval(this.intervalId_)
+            this.intervalId_ = null
         }
     }
 }
 
 let GameTimer = new Timer(90);
-let interval = setInterval(GameTimer.startTimer(), 1000);
 
 class GameManager {
     constructor(mode) {
@@ -52,6 +58,20 @@ class GameManager {
 
     }
 
+    reset() {
+        this.mode_ = "Select";
+        this.teamOneScore_ = 0;
+        this.teamTwoScore_ = 0;
+
+        this.playingRound_ = false;
+        this.category_ = null;
+        this.catgoryItr_ = 0;
+        this.categoryArray = null;
+
+        document.getElementById("textBar").innerHTML = "Select Category";
+
+    }
+
 
     getTeamOneScore() { return this.teamOneScore_; }
     
@@ -64,7 +84,6 @@ class GameManager {
             this.categoryItr_ = (this.categoryItr_ + 1) % CATEGORY_MAP.length;
         } else if (this.mode_ == "Round") {
             document.getElementById("textBar").innerHTML = this.categoryArray_[Math.floor(Math.random() * this.categoryArray_.length)];
-            
         }
     }
 
@@ -74,41 +93,48 @@ class GameManager {
             selectText = document.getElementById("textBar").innerHTML;
             if(selectText == "Select Category") {
                 alert("Please Select a Category");
-            } else {
-                this.category_ = selectText;
 
-                this.categoryArray_ = CATEGORIES[CATEGORY_MAP.find((element)=>{ return element[0] == selectText;})[1]];
+            } else {
+                if(!this.categoryArray_) {
+                    this.category_ = selectText;
+                    this.categoryArray_ = CATEGORIES[CATEGORY_MAP.find((element)=>{ return element[0] == selectText; })[1]];
+                }
+                this.startRound();
+                
+                document.getElementById("textBar").innerHTML = this.categoryArray_[Math.floor(Math.random() * this.categoryArray_.length)];
                 
                 this.mode_ = "Round"
-                GameTimer.startTimer();
-                document.getElementById("textBar").innerHTML = CATEGORY_MAP[this.categoryItr_][0];
-                this.categoryItr_ = (this.categoryItr_ + 1) % CATEGORY_MAP.length;
     
             }    
+
         } else if (this.mode_ == "Round") {
-            document.getElementById("textBar").innerHTML = "||"
+            document.getElementById("textBar").innerHTML = "| |"
             
+
+            this.stopRound();
 
             this.mode_ = "Select";
         }
     }
 
+    startRound() {
+        this.playingRound_ = true;
+        GameTimer.start();
+        document.getElementById("nextButton").disabled = false;
 
-
-}
-
-
-function timer(timerLength = 5000) {
-    timeLeft = timerLength;
-}
-
-
-
-function reset() {
-    GameMan = new GameManager("Select");
-    document.getElementById("textBar").innerHTML = "Select Category";
+    }
+    
+    stopRound() {
+        this.playingRound_ = false;
+        GameTimer.stop();
+        document.getElementById("nextButton").disabled = true;
+    }
 
 }
+
+
+
+
 let GameMan;
 function main() {
     GameMan = new GameManager("Select");
